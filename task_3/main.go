6c9c7f049f97c3a0f8e84ec364e97fad07a2b5f1
olegg <gusev.oleg04@gmail.com>
2025-09-09 00:27:05 +0300

    task #3 completed

---
+++ task_3/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+func worker(id int, jobs <-chan int) {
+	for job := range jobs {
+		fmt.Printf("Worker %d получил: %d\n", id, job)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Укажите количество воркеров, например: go run main.go 3")
+		return
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+	if err != nil || n <= 0 {
+		fmt.Println("Некорректное количество воркеров")
+		return
+	}
+
+	jobs := make(chan int)
+
+	for i := 1; i <= n; i++ {
+		go worker(i, jobs)
+	}
+
+	for i := 1; ; i++ {
+		jobs <- i // пишем в канал
+		time.Sleep(time.Second)
+	}
+}
