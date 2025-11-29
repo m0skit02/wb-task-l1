d2f47347e6a276f36b956d7703dd6ed989dcd17f
olegg <gusev.oleg04@gmail.com>
2025-11-29 22:56:54 +0300

    task #20 completed

---
+++ task_20/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverseRunes(r []rune, l, rgt int) {
+	for l < rgt {
+		r[l], r[rgt] = r[rgt], r[l]
+		l++
+		rgt--
+	}
+}
+
+func reverseWords(s string) string {
+	r := []rune(s)
+
+	// 1. Разворачиваем всю строку
+	reverseRunes(r, 0, len(r)-1)
+
+	// 2. Разворачиваем каждое слово
+	start := 0
+	for i := 0; i <= len(r); i++ {
+		if i == len(r) || r[i] == ' ' {
+			reverseRunes(r, start, i-1)
+			start = i + 1
+		}
+	}
+
+	return string(r)
+}
+
+func main() {
+	input := "snow dog sun"
+	result := reverseWords(input)
+	fmt.Println(result) // sun dog snow
+}
