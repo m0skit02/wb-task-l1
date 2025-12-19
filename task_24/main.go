fa8f533077b0c1f5fa723368705ddf270610ad1e
olegg <gusev.oleg04@gmail.com>
2025-12-20 02:49:51 +0300

    task #24 completed

---
+++ task_24/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	x float64
+	y float64
+}
+
+func NewPoint(x, y float64) Point {
+	return Point{x: x, y: y}
+}
+
+func (p Point) Distance(other Point) float64 {
+	dx := other.x - p.x
+	dy := other.y - p.y
+	return math.Sqrt(dx*dx + dy*dy)
+}
+
+func main() {
+	p1 := NewPoint(0, 0)
+	p2 := NewPoint(3, 4)
+
+	distance := p1.Distance(p2)
+	fmt.Println(distance)
+}
